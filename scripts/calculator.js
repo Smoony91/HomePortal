a0e86ff6893c2d6962fb69dfaba6a3ec8d68c34e
Smoony91 <m.sereda_91@mail.ru>
2020-06-28 11:41:13 +0300

    исправлен html и дабавлены элементы css для калькулятора

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,16 +1,31 @@
 const inputCalc = document.querySelector('.account')
 const resulCalc = document.querySelector('.result')
 
-function addSymToEquation(i) {
-    inputCalc.value = inputCalc.value + i
+function addSymToEquation(sym) {
+    inputCalc.value = inputCalc.value + sym
 }
 
 function result() {
-   resulCalc.value = eval(inputCalc.value)
-   inputCalc.value = eval(inputCalc.value)
+    
+    if  (inputCalc.value !== '') { 
+        resulCalc.value = eval(inputCalc.value)
+        inputCalc.value = eval(inputCalc.value)
+    }
+   
+   
 }
 
 function reset() {
     resulCalc.value = ''
     inputCalc.value = ''
+}
+
+function deleteLastSym() {
+
+    inputCalc.value = inputCalc.value.slice(0,-1)
+    // var str1 = 'Приближается утро.';
+    // var str2 = str1.slice(4, -2);
+    
+    // console.log(str2); // ВЫВОД: лижается утр
+    
 }
