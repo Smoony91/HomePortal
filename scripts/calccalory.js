60dbca19d9d514611354d9e9bbc1877aa792cacb
Smoony91 <m.sereda_91@mail.ru>
2020-07-04 11:22:17 +0300

    начало разработки калькулятора калорий

---
+++ scripts/calccalory.js
@@ -0,0 +1,16 @@
+
+
+function inputAllCalorys () {
+var num1 = document.getElementById('input100gram')
+    num1 = parseInt(num1)
+var num2 = document.querySelector('.inputAllcalory')
+var num3 = document.querySelector('.inputResoult')
+var num4 =  num1 / 100 * num2
+
+console.log (parseInt(num1))
+console.log (num2)
+console.log (num4)
+}
+// 50
+// 150
+// 50/150
