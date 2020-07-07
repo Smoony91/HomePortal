4d171a70c2ee3981875cc29bfcbd7b0739556036
Smoony91 <m.sereda_91@mail.ru>
2020-07-07 22:02:07 +0300

    Доработан калькулятор калорий

--- scripts/calccalory.js
+++ scripts/calccalory.js
@@ -1,15 +1,18 @@
 
 
 function inputAllCalorys () {
-var num1 = document.getElementById('n1')
-    num1.value = parseInt()
-var num2 = document.querySelector('.inputAllcalory')
-var num3 = document.querySelector('.inputResoult')
-var num4 =  num1 / 100 * num2
-
-console.log (parseInt(num1))
+let num1 = document.getElementById('n1')
+    num1 = parseInt(n1.value)
+let num2 = document.getElementById('n2')
+    num2 = parseInt(n2.value)
+let num3 = document.getElementById('n3')
+    num3 = parseInt(n3.value)
+let num4 =  num1 / 100 * num2
+n3.value= num4
+console.log (num1)
 console.log (num2)
 console.log (num4)
+
 }
 // 50
 // 150
