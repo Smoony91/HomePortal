9371c4e723203d6a6f2cde46593a90ccfe254018
Smoony91 <m.sereda_91@mail.ru>
2020-07-25 12:02:55 +0300

    добавлены файлы с заданий c курсов

--- scripts/calccalory.js
+++ scripts/calccalory.js
@@ -7,8 +7,8 @@ let num2 = document.getElementById('n2')
     num2 = parseInt(n2.value)
 let num3 = document.getElementById('n3')
     num3 = parseInt(n3.value)
-let num4 =  num1 / 100 * num2
-n3.value= num4
+let num4 =  num1/ 100 * num2
+n3.value = num4
 console.log (num1)
 console.log (num2)
 console.log (num4)
