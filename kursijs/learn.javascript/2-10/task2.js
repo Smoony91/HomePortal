5fb9558934589b9fdd1681a86bd5950dfc8d6a2b
Smoony91 <m.sereda_91@mail.ru>
2020-08-08 20:27:41 +0300

    добавлены файлы с заданий c курсов

---
+++ kursijs/learn.javascript/2-10/task2.js
@@ -0,0 +1,11 @@
+let question = prompt ("Укажи любое число,сударь")
+
+if (question == 0) {
+    alert (0)
+}
+else if (question < 0) {
+    alert (-1)
+}
+else if (question > 0) {
+    alert (1)
+}
