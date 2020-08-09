1e5906fb7036d1f89f90d3fc7fba1218e3618171
Smoony91 <m.sereda_91@mail.ru>
2020-08-09 10:13:59 +0300

    добавлены файлы с заданий c курсов

--- kursijs/learn.javascript/2-11/task3.js
+++ kursijs/learn.javascript/2-11/task3.js
@@ -1,1 +1,22 @@
-let login = promt ("Кто там?")
+let login = prompt ("Кто там?") 
+if (login == 'Админ') {
+
+    let pass = prompt ('Ведите пароль');
+
+    if (pass === "Я главный") {
+        alert ('Здравствуйте!')   
+    }
+    else if (pass == '' || pass == null) {
+        alert ("Отменено")
+    }
+    else {
+        alert ('Неверный пароль!')
+    }
+     
+}
+else if (login == '' || login == null) {
+    alert ('отмена логина!');
+ }
+else  {
+   alert ("Я вас не знаю!");
+}
